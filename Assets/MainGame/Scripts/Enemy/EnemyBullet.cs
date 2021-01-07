5a900341c6c00bba24e596fb37130dc07075c897
baophuc53 <55937237+baophuc53@users.noreply.github.com>
2021-01-08 00:02:52 +0700

    add enemy prefab

---
+++ Assets/MainGame/Scripts/Enemy/EnemyBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.magnitude > 500.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+}
