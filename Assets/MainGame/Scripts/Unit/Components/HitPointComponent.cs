14ba4a8cc9adfcbee96304168761cd24e87046ef
DESKTOP-NBAJJE3\Admin <duyquangnvx@gmail.com>
2021-01-20 01:52:56 +0700

    fix file direction

---
+++ Assets/MainGame/Scripts/Unit/Components/HitPointComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HitPointComponent : BaseComponent
+{
+    public float HitPoint { get; set; }
+    public float shiel { get; set; }
+
+
+    public override void InitComponents()
+    {
+        base.InitComponents();
+    }
+}
